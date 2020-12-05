200b76e108cc992a4002985bc54c4bf09724f1fe
Joelle Kado <jkado@my.hpu.edu>
2020-12-05 15:12:25 -0600

    Imported GalleryList into App ; Created imageProp ; Created GalleryItem Component ; Imported GalleryItem into GalleryList

---
+++ src/components/GalleryItem/GalleryItem.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class GalleryItem extends Component{
+
+  
+  render(){
+
+  return(
+      
+      
+      <h1>hello from GalleryItem</h1>
+      ) // end return
+    } // end render
+} // end class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default GalleryItem;
