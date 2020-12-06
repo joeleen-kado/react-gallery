92eb98e9a2a792cf79abe2b65eb59b9dca52c0ef
Joelle Kado <jkado@my.hpu.edu>
2020-12-05 22:31:45 -0600

    images toggle with buttonClick

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 class GalleryItem extends Component{
 
   state = ({
-    likes  : 0
+    likes  : 0,
+    showPicture : false
   })
+  
+  togglePicture = () => {
+      this.setState({
+        showPicture: !this.state.showPicture // flip the boolean using ! NOT
+      })
+    }
 
     
 
@@ -14,12 +21,26 @@ class GalleryItem extends Component{
   return(
       <div>
           <h2>{image.title}</h2>
+          
+
+          
+          
           <h4>{image.description}</h4>
-          <img src={image.path}/>
+      
+      
+        
+        
+          <img src={this.state.showPicture && image.path }/>
+        
+          {/* <img src={image.path}/>  */}
+        
+        
+        
           <h3>Likes: {image.likes}</h3>
           <h3>{image.id}</h3>
         <button onClick={(event)=>this.props.likePutExtensionProp(event, image.id)}>LIKE</button>
-        
+        <button onClick={this.togglePicture}>{ this.state.showPicture? `SEE DESCRIPTION` : `SEE IMAGE` }
+        </button>
       </div>
       ) // end return
     } // end render
