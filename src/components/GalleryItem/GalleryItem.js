92f44060f31a3bd46f6961ad9106f1ba953e411c
Joelle Kado <jkado@my.hpu.edu>
2020-12-05 23:50:07 -0600

    Formatted Code

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 class GalleryItem extends Component{
-
   state = ({
     likes  : 0,
     showPicture : false
@@ -11,54 +10,27 @@ class GalleryItem extends Component{
       this.setState({
         showPicture: !this.state.showPicture // flip the boolean using ! NOT
       })
-    }
-
-    
+    };//END togglePicture
 
   render(){
     let image = this.props.imageProp;
     
-  return(
-      <div>
-          <h2>{image.title}</h2>
-          
-
-          
-          
-      
-      
-        
-        {this.state.showPicture? <img src={image.path}/> :   <h4>{image.description}</h4>}
-
-        {/*  <img src={this.state.showPicture && image.path }/> */}
-        
-          {/* <img src={image.path}/>  */}
-        
-        
-        
-          <h3>Likes: {image.likes}</h3>
-          <h3>{image.id}</h3>
-        <button onClick={(event)=>this.props.likePutExtensionProp(event, image.id)}>LIKE</button>
-        <button onClick={this.togglePicture}>{ this.state.showPicture? `SEE DESCRIPTION` : `SEE IMAGE` }
+    return(
+      <>
+        <h2>{image.title}</h2>
+            {this.state.showPicture? <img src={image.path}/> :   
+              <h4>{image.description}</h4>
+            }
+        <h3>Likes: {image.likes}</h3>
+        <h3>{image.id}</h3>
+        <button onClick={(event)=>
+          this.props.likePutExtensionProp(event, image.id)}>LIKE
         </button>
-      </div>
-      ) // end return
-    } // end render
+        <button onClick={this.togglePicture}>{ this.state.showPicture? 
+          `SEE DESCRIPTION` : `SEE IMAGE` }
+        </button>
+      </>
+    ) // end return
+  } // end render
 } // end class
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 export default GalleryItem;
