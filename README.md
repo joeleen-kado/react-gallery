76088d68a30e556c7803e0a147f2198fb76ee57f
Joelle Kado <jkado@my.hpu.edu>
2020-12-06 22:43:05 -0600

    README COMPLETE ; Added more images

--- README.md
+++ README.md
@@ -1,9 +1,36 @@
-# Project Name
+![REPO SIZE](https://img.shields.io/github/repo-size/JoelleKado/react-gallery.svg?style=flat-square)
+![TOP_LANGUAGE](https://img.shields.io/github/languages/top/JoelleKado/react-gallery.svg?style=flat-square)
+![FORKS](https://img.shields.io/github/forks/JoelleKado/react-gallery.svg?style=social)
 
-[Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
+# REACT GALLERY
 
-## Description
+## The Gallery of your life
 
-Your project description goes here. What problem did you solve? How did you solve it?
+_Duration: 15hrs_
 
-Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+I am Full Stack Engineer. But overarching that--along with every other aspect of my life--, I am an artist. We artists rely on self advertisement, if the desire is to land work. The online Gallery enables the Visual Artist/Full Stack Engineer to have a consistent and efficient gallery of the entirety of their collection.
+
+## Screen Shot
+![intro](public/images/screenShots/screenShot.png)
+
+### Prerequisites
+
+- [Node.js](https://nodejs.org/en/)
+- [React.js](https://reactjs.org/)
+
+## Installation
+
+1. Clone the https://github.com/JoelleKado/react-gallery.git repository to your local machine.
+2. navigate to `public/images` folder. This is your portfolio. Insert your work in .jpg form.
+
+## Enter the Gallery
+
+1. In Terminal 1: run commmand: `npm run server`: This will open the Gallery
+2. In Terminal 2: run commmand: `npm run client`: This will enter you into the Gallery; The Virtual Gallery will open in your default web browser.
+3. Enjoy. Have your friends view your gallery. They can `like` your works, and, toggle an artist's description for each work.
+
+## Acknowledgement
+Thanks to [Prime Digital Academy](www.primeacademy.io) who equipped and helped me to make this application a reality. Thank you https://github.com/freder48, https://github.com/bradybaker: Your collaboration with me on client-to-server communication was tantamount to the Gallery being likable. 
+
+## Support
+If you have suggestions or issues, please email me at [jkado@my.hpu.edu](www.google.com)
--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,15 +1,14 @@
 const galleryItems = [
-    //{ id: 1, path: 'images/goat_small.jpg', title: 'Goat', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
     { id: '1', path: "images/joelleObject.jpg", title: 'Joelle.Object', description: 'Joelle as an object.', likes: 0 },
-    { id: '2', path: "images/meAtTrail.jpg", title: 'Prairie Trail', description: 'A trail in Wyoming, MN.', likes: 0 },
-    { id: '3', path: "images/octopus.jpg", title: 'Octopus', description: 'My inky Octopus.', likes: 0 },
-    { id: '5', path: "images/coolForSchool.jpg", title: 'Cool for School', description: 'Stay in School.', likes: 0 },
-    { id: '6', path: "images/marshallTerraceOne.jpg", title: 'Marshall Terrace One', description: 'I spend my a Summer day painting at the Mississippi.', likes: 0 },
-    { id: '7', path: "images/riverScape.jpg", title: 'RiverScape', description: "The Lowry Bridge from one of my riverside parks.", likes: 0 },
-    { id: '8', path: "images/bootCamp.jpg", title: 'Boot Camp', description: "At 19, I was fortunate enough to wear my Nation's Uniform.", likes: 0 }
-
-
-
+    { id: '2', path: "images/blackAndWhite19.jpg", title: 'Black and White 2019', description: 'Every year I showcase at the Hallberg Cener for the Arts, in Wyoming, MN.', likes: 0 },
+    { id: '3', path: "images/meAtTrail.jpg", title: 'Prairie Trail', description: 'A trail in Wyoming, MN.', likes: 0 },
+    { id: '4', path: "images/octopus.jpg", title: 'Octopus', description: 'My inky Octopus.', likes: 0 },
+    { id: '5', path: "images/hawaii.jpg", title: 'Big Island', description: 'My mom, and older brother came to visit me for College Graduation', likes: 0 },
+    { id: '6', path: "images/coolForSchool.jpg", title: 'Cool for School', description: 'Stay in School.', likes: 0 },
+    { id: '7', path: "images/tartuffe.jpg", title: 'Tartuffe', description: "I played 'Damis', in Paul and Vi Loo Theatre's presentation of Moliere's 'Tartuffe'; Directed by Joyce Maltby.", likes: 0 },
+    { id: '8', path: "images/marshallTerraceOne.jpg", title: 'Marshall Terrace One', description: 'I spend many a Summer day painting at the Mississippi.', likes: 0 },
+    { id: '9', path: "images/riverScape.jpg", title: 'RiverScape', description: "The Lowry Bridge from one of my riverside parks.", likes: 0 },
+    { id: '10', path: "images/bootCamp.jpg", title: 'Boot Camp', description: "At 19, I was fortunate enough to wear my Nation's Uniform.", likes: 0 }
 ];
 
 module.exports = galleryItems;
